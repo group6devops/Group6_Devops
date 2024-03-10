2c7fd60af99931e9aa1ace3e91c2c2818eac41b8
kvenkatt87 <162661920+kvenkatt87@users.noreply.github.com>
2024-03-10 16:17:39 +0530

    Update FlightController.java

--- src/main/java/com/travelbooking/flight/controller/FlightController.java
+++ src/main/java/com/travelbooking/flight/controller/FlightController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.travelbooking.flight.entity.Flight;
 import com.travelbooking.flight.service.FlightService;
 
+import java.util.*;
 
 @RestController
 @RequestMapping("/flights")
