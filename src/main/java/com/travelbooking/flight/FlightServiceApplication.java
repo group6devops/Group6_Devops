015e68e4c74ee717f6bb3f20f87322a3b7af29f7
kvenkatt87 <162661920+kvenkatt87@users.noreply.github.com>
2024-03-10 11:27:28 +0530

    Update FlightServiceApplication.java for linttest

--- src/main/java/com/travelbooking/flight/FlightServiceApplication.java
+++ src/main/java/com/travelbooking/flight/FlightServiceApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FlightServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlightServiceApplication.class, args);
-	}
+public static void main(String[] args)
+{
+SpringApplication.run(FlightServiceApplication.class, args);
+}
 
 }
